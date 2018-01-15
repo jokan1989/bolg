77917ae40dc8741b02768fbd001f8ac258ba1fa5
jokan1989 <jokan1989@outlook.com>
2018-01-15 20:38:03 +0800

    add login

--- src/main/java/com/jokan/bolg/web/BaseController.java
+++ src/main/java/com/jokan/bolg/web/BaseController.java
@@ -1,10 +1,15 @@
 package com.jokan.bolg.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value="/base")
 public class BaseController {
 	
+	@RequestMapping
+	public String toLogin(ModelMap map) {
+		
+		return "";
+	}
 }
